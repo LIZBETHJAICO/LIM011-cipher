f76c701ede372040f66ed10d60cefe432ef1515c
lizjaico <Lizbethjaico@gmail.com>
2019-10-14 19:11:13 -0500

    cipher v1

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,71 @@
 window.cipher = {
   // Acá escribe tu código.
+  encode: (num, datos) => {
+    let datoscifrados = "";
+    for (let i = 0; i < datos.length; i++) {
+      let convertAscii = datos.charCodeAt(i);
+
+      //1- MAYÚSCULA  2- MINÚSCULA  3- NÚMERO segun codigo ASCII
+      //console.log(convertAscii);
+      if (convertAscii >= 65 && convertAscii <= 90) {
+        //para mayusculas
+        datoscifrados += String.fromCharCode(
+          ((convertAscii - 65 + num) % 26) + 65
+        );
+      } else if (convertAscii >= 97 && convertAscii <= 122) {
+        // para minusculas
+        datoscifrados += String.fromCharCode(
+          ((convertAscii - 97 + num) % 26) + 97
+        );
+      } else if (convertAscii >= 48 && convertAscii <= 57) {
+        // para nmeros del 0 al 9
+        datoscifrados += String.fromCharCode(
+          ((convertAscii - 48 + num) % 10) + 48
+        );
+      } else if (convertAscii === 241) {
+        datoscifrados += '*';
+      } else if (convertAscii === 209) {
+        datoscifrados += '*';
+      } else {
+        datoscifrados += datos.charAt(i);
+      }
+      // debugger
+      //document.getElementById('datoscifrados').value = datoscifrados;
+    }
+    return datoscifrados;
+  },
+
+  decode: (num, datos) => {
+    let datoscifrados = "";
+    for (let i = 0; i < datos.length; i++) {
+      let convertAscii = datos.charCodeAt(i);
+
+      //1- MAYÚSCULA  2- MINÚSCULA  3- NÚMERO segun codigo ASCII
+
+      if (convertAscii >= 65 && convertAscii <= 90) {
+        //para mayusculas
+        datoscifrados += String.fromCharCode(
+          ((convertAscii - 90 - num) % 26) + 90
+        );
+      } else if (convertAscii >= 97 && convertAscii <= 122) {
+        // para minusculas
+        datoscifrados += String.fromCharCode(
+          ((convertAscii - 97 - num+26*2)% 26) + 97
+        );
+      } else if (convertAscii >= 48 && convertAscii <= 57) {
+        // para nmeros del 0 al 9
+        datoscifrados += String.fromCharCode(
+          ((convertAscii - 57 - num) % 10) + 57
+        );
+      }  else {
+        datoscifrados += datos.charAt(i);
+      }
+
+      //document.getElementById('datoscifrados}').value = datoscifrados;
+    }
+    return datoscifrados;
+  },
+
+  
+  
 };
---
+++ src/index.js
@@ -0,0 +1,84 @@
+// sidebar toggle
+// trayendo variables DOM
+//Llamar todo lo que quiero dle formulario
+let btncifrar = document.getElementById("cifrado");
+let btndescifrar = document.getElementById("descifrado");
+let  ndesplazamiento = document.getElementById("num");
+let  nombresector = document.getElementById("nombresector");
+let  ipordenador = document.getElementById("ipordenador");
+let contraseña=document.getElementById("contraseña");
+let btniniciar =document.getElementById('hello');
+let barradecode=document.getElementById('barradecode');
+let barraencode=document.getElementById('barraencode');
+let btndescifrado = document.getElementById("btndescifrado");
+let  desplazar = document.getElementById("desplazar");
+let  pcifrada = document.getElementById("Pcifrada");
+let  btnlimpiar = document.getElementById("limpiar");
+
+// crear eventos 
+
+btncifrar.addEventListener('click', cifrado);
+function cifrado() {
+  let nombresectorcifrar = nombresector.value;
+  let ipordenadorcifrar = ipordenador.value;
+  let contraseñacifrar=contraseña.value;
+  let datos = (nombresectorcifrar +"\n"+ipordenadorcifrar+ "\n"+ contraseñacifrar);
+  let num = parseInt(ndesplazamiento.value);
+  let  datoscifrados = window.cipher.encode(num, datos);
+ document.getElementById('datoscifrados').innerHTML = datoscifrados;
+
+}
+
+btndescifrar.addEventListener('click', descifrado);
+function descifrado() {
+  let nombresectorcifrar = nombresector.value;
+  let ipordenadorcifrar = ipordenador.value;
+  let contraseñacifrar=contraseña.value;
+  let datos = (nombresectorcifrar + "\n" + ipordenadorcifrar + " \n"+ contraseñacifrar);
+  let num = parseInt(ndesplazamiento.value);
+  
+  let  datoscifrados = window.cipher.decode(num, datos);
+  document.getElementById('datoscifrados').innerHTML = datoscifrados;
+}
+btndescifrado.addEventListener('click', descifrador);
+function descifrador() {
+  let datos2 = pcifrada.value;
+  
+  let num2 = parseInt(desplazar.value);
+  
+  let  datoscifrados = window.cipher.decode(num2, datos2);
+  document.getElementById('Pdescifrada').innerHTML = datoscifrados;
+}
+
+
+btniniciar.addEventListener('click',iniciar);
+function iniciar(){
+  document.getElementById('welcome').classList.add('ocultar');
+  document.getElementById('barras').classList.remove('ocultar');
+  document.getElementById('toshow').classList.remove('ocultar');
+}
+barradecode.addEventListener('click',decode);
+function decode(){
+  document.getElementById('welcome').classList.add('ocultar');
+  document.getElementById('barras').classList.remove('ocultar');
+  document.getElementById('toshow').classList.remove('ocultar');
+  document.getElementById('coder').classList.remove('ocultar');
+  document.getElementById('coder1').classList.add('ocultar');
+}
+
+
+barraencode.addEventListener('click',encode);
+function encode(){
+  document.getElementById('coder').classList.add('ocultar');
+  document.getElementById('coder1').classList.remove('ocultar');
+  
+}
+
+btnlimpiar.addEventListener('click',limpiar);
+function limpiar(){
+nombresector.value="";
+ipordenador.value="";
+contraseña.value="";
+ndesplazamiento.value="";
+ 
+}
